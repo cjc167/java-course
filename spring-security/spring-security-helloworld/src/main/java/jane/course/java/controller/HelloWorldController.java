eaceccf20e6122396fed8315505139640e05df35
ZhangTieJun <zhangtiejun@gmf-inc.com>
2016-02-29 15:20:47 +0800

    增加Spring Security模块

---
+++ spring-security/spring-security-helloworld/src/main/java/jane/course/java/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package jane.course.java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping(value = {"/", "index.html"})
+	public String index(){
+		return "index";
+	}
+}
