2810c797384a6f7a977dd5374deb2f279a0fc75c
张铁军 <zhangtiejun@gmf-inc.com>
2016-04-25 11:38:25 +0800

    设置响应头

--- spring-mvc-ajax-submit/src/main/java/jane/course/java/spring/mvc/controller/HelloWorldController.java
+++ spring-mvc-ajax-submit/src/main/java/jane/course/java/spring/mvc/controller/HelloWorldController.java
@@ -2,10 +2,12 @@ package jane.course.java.spring.mvc.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -36,8 +38,11 @@ public class HelloWorldController {
 	}
 	
 	
+	
+	
 	@RequestMapping(value = "/ajax/post", method = RequestMethod.GET)
 	public String post(Model model, String username){
+		logger.info("Get Method.");
 		if(username == null || "".equals(username.trim())){
 			username = "World!";
 		}
@@ -50,13 +55,15 @@ public class HelloWorldController {
 	public ResponseEntity<String> post(User user){
 		logger.info(String.valueOf(user));
 		
-		HttpStatus httpStatus = HttpStatus.OK;
+		HttpStatus httpStatus = HttpStatus.CREATED;
 		
 		if("jane".equals(user.getUsername())) {
 			httpStatus = HttpStatus.CONFLICT;
 		}
+		MultiValueMap<String, String> headers = new HttpHeaders();
+		headers.add("Content-Type", "text/plain;charset=UTF-8");
 		
-		ResponseEntity<String> response = new ResponseEntity<String>(new Gson().toJson(user), httpStatus);
+		ResponseEntity<String> response = new ResponseEntity<String>(new Gson().toJson(user),  headers, httpStatus);
 		return response;
 	}
 }
